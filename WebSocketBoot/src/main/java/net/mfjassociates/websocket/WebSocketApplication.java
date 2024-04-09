fede271eb2839b0b41a478fcc9c24e322c00f8db
Jauvin, Mario <mfj@earthling.net>
2024-04-09 17:14:00 -0400

    Use an application truststore instead of the JDK
    For this test, emptyStore.keystore is a jks keystore that contains only
    a unused private key/public key pair.  There are no CA certificates of
    any kind so it should fail any validation.
    
    The WsWebSocketContainer uses the trust store and trust store password
    properties from config user properties, not from system properties.  As
    such I am creating them from the spring boot standard web socket client.
    Since the trust store property must be a file object and since this code
    is running in a jar, I cannot use an IDE/maven relative path for the
    keystore.  The keystore must be packaged in the jar and at runtime
    copied to a folder. The trust store property will then be set to that
    temporary file location.
    
    The temporary folder is created in the main method before starting the
    spring boot and passed as an argument.

--- WebSocketBoot/src/main/java/net/mfjassociates/websocket/WebSocketApplication.java
+++ WebSocketBoot/src/main/java/net/mfjassociates/websocket/WebSocketApplication.java
@@ -1,13 +1,25 @@
 package net.mfjassociates.websocket;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebSocketApplication {
+	
+	public static final String TEMP_TRUST_STORE = "trust-store-temp-folder";
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebSocketApplication.class, args);
-    }
-
+	public static void main(String[] args) throws IOException {
+		SpringApplication app=new SpringApplication(WebSocketApplication.class);
+		Path tempDirectory = Files.createTempDirectory("trust");
+		tempDirectory.toFile().deleteOnExit();
+		Properties defaults=new Properties();
+		defaults.put(TEMP_TRUST_STORE, tempDirectory.toString());
+		app.setDefaultProperties(defaults);
+		app.run(args);
+	}
 }
--- WebSocketBoot/src/main/java/net/mfjassociates/websocket/controller/WebSocketController.java
+++ WebSocketBoot/src/main/java/net/mfjassociates/websocket/controller/WebSocketController.java
@@ -1,8 +1,18 @@
 package net.mfjassociates.websocket.controller;
-
+import static net.mfjassociates.websocket.WebSocketApplication.TEMP_TRUST_STORE;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.tomcat.websocket.Constants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.TextMessage;
@@ -13,10 +23,25 @@ import net.mfjassociates.websocket.SimpleTextWebSocketHandler;
 
 @RestController
 public class WebSocketController {
+	
+	private static final String KEYSTORE_NAME = "emptyStore.keystore";
+	
+	@Value("${"+TEMP_TRUST_STORE+"}")
+	private String tempFolder;
 
     @GetMapping("/hello")
     public String hello() throws ExecutionException, InterruptedException, IOException {
         StandardWebSocketClient client = new StandardWebSocketClient();
+        // copy the keystore from the classpath to a temp folder.  The SSL trust store must be a java.io.File
+        URL ks = this.getClass().getResource("/"+KEYSTORE_NAME);
+        Path ksp = Paths.get(tempFolder).resolve(KEYSTORE_NAME);
+        try (InputStream in = ks.openStream()) {
+            Files.copy(in, ksp, StandardCopyOption.REPLACE_EXISTING);
+        }
+        Map<String, Object> trustStoreProperties=new HashMap<>();
+        trustStoreProperties.put(Constants.SSL_TRUSTSTORE_PROPERTY, ksp.toAbsolutePath().toString());
+        trustStoreProperties.put(Constants.SSL_TRUSTSTORE_PWD_PROPERTY, "storePassword");
+        client.setUserProperties(trustStoreProperties);
         WebSocketSession session = client.doHandshake(new SimpleTextWebSocketHandler(), "wss://echo.websocket.org").get();
 
         session.sendMessage(new TextMessage("Hello Server"));
