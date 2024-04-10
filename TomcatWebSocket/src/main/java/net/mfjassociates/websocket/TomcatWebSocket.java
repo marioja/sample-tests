3cd09016ba0b8ff96b9e41030e7e4b02f39b3fe7
Mario Jauvin <marioja@users.noreply.github.com>
2024-04-10 19:08:28 -0400

    Added required dependency and custom truststore

--- TomcatWebSocket/src/main/java/net/mfjassociates/websocket/TomcatWebSocket.java
+++ TomcatWebSocket/src/main/java/net/mfjassociates/websocket/TomcatWebSocket.java
@@ -1,20 +1,26 @@
 package net.mfjassociates.websocket;
 
-import java.net.URI;
+import org.apache.tomcat.websocket.Constants;
 
-import javax.websocket.ClientEndpoint;
-import javax.websocket.ContainerProvider;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.WebSocketContainer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.websocket.*;
 
 @ClientEndpoint
-public class TomcatWebSocket {
+public class TomcatWebSocket extends Endpoint {
     private static Object waitLock = new Object();
 
-    @OnOpen
-    public void onOpen(Session session) {
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
         try {
             session.getBasicRemote().sendText("Hello");
         } catch (Exception e) {
@@ -26,14 +32,31 @@ public class TomcatWebSocket {
     public void onMessage(String message) throws InterruptedException {
         System.out.println("Received msg: " + message);
         synchronized(waitLock) {
-        	waitLock.wait();
+        	waitLock.notify();
         }
     }
+    private static final String KEYSTORE_NAME = "emptyStore.keystore";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path tempDirectory = Files.createTempDirectory("trust");
+        tempDirectory.toFile().deleteOnExit();
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        // Create a configuration object
+        ClientEndpointConfig config = ClientEndpointConfig.Builder.create().build();
+
+        // copy the keystore from the classpath to a temp folder.  The SSL trust store must be a java.io.File
+        URL ks = TomcatWebSocket.class.getResource("/"+KEYSTORE_NAME);
+        Path ksp = tempDirectory.resolve(KEYSTORE_NAME);
+        try (InputStream in = ks.openStream()) {
+            Files.copy(in, ksp, StandardCopyOption.REPLACE_EXISTING);
+        }
+        // Set user properties
+        Map<String, Object> trustStoreProperties=new HashMap<>();
+        trustStoreProperties.put(Constants.SSL_TRUSTSTORE_PROPERTY, ksp.toAbsolutePath().toString());
+        trustStoreProperties.put(Constants.SSL_TRUSTSTORE_PWD_PROPERTY, "storePassword");
+        config.getUserProperties().putAll(trustStoreProperties);
         try {
-            Session session = container.connectToServer(TomcatWebSocket.class, new URI("ws://echo.websocket.org"));
+            Session session = container.connectToServer(TomcatWebSocket.class, config, new URI("ws://echo.websocket.org"));
             synchronized(waitLock) {
                 waitLock.wait();
             }
